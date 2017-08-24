7eedd873979412041c5db6d1c89efd8a4a3ce9aa
xboy <dolphin2525@163.com>
2017-08-24 18:51:23 +0800

    提交工程文件

---
+++ OBAKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OBAKit
+//
+//  Created by leeqb on 24/08/2017.
+//  Copyright © 2017 leeqb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OBAKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OBAKit
+//
+//  Created by leeqb on 24/08/2017.
+//  Copyright © 2017 leeqb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
